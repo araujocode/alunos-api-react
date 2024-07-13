8b137f2bddf21365b864da7e7011e09baff93ef0
Bruno Araujo <bruno.cadoma@gmail.com>
2024-07-13 05:09:24 -0300

    chore: apply migrations and seed initial data in Program.cs

--- AlunoApi/Context/AppDbContext.cs
+++ AlunoApi/Context/AppDbContext.cs
@@ -1,6 +1,35 @@
-﻿namespace AlunosApi.Context
+﻿using AlunosApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlunosApi.Context
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {            
+        }
+
+        public DbSet<Aluno> Alunos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Aluno>().HasData(
+                new Aluno
+                {
+                    Id = 1,
+                    Nome = "Maria da Penha",
+                    Email = "mariapenha@yahoo.com",
+                    Idade = 21
+                },
+                new Aluno
+                {
+                    Id = 2,
+                    Nome = "Manuel Bueno",
+                    Email = "manuelbueno@yahoo.com",
+                    Idade = 22
+                }
+            );
+        }
+
     }
 }
